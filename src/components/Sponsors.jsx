110f940053daa3b53eece4b93e816bfd5427e930
AI31 <bohdan.pavlyk.mknssh.2023@lpnu.ua>
2025-12-17 19:21:54 +0200

    Changes in Sponsors block

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -35,15 +35,15 @@ const Sponsors = () => {
         <div className="sponsors">
             <div className="slider-container">
                 <Slider {...settings}>
-                    <div><div className='sponsors__img sponsors__img2'></div></div>
+                    <div><div className='sponsors__img sponsors__img3'></div></div>
                     <div><div className='sponsors__img sponsors__img4'></div></div>
+                    <div><div className='sponsors__img sponsors__img5'></div></div>
+                    <div><div className='sponsors__img sponsors__img6'></div></div>
                     <div><div className='sponsors__img sponsors__img7'></div></div>
                     <div><div className='sponsors__img sponsors__img8'></div></div>
                     <div><div className='sponsors__img sponsors__img9'></div></div>
-                    <div><div className='sponsors__img sponsors__img10'></div></div>
-                    <div><div className='sponsors__img sponsors__img11'></div></div>
                     <div><div className='sponsors__img sponsors__img12'></div></div>
-                    <div><div className='sponsors__img sponsors__img13'></div></div>
+                    <div><div className='sponsors__img sponsors__img14'></div></div>
                 </Slider>
             </div>
         </div>
